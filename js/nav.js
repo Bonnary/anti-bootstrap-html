2420fe1be7bc0e674ff3c71de87014abbcf949f1
Bonnary <69995568+Bonnary@users.noreply.github.com>
2022-05-30 13:33:18 +0700

    Navigation Bar

---
+++ js/nav.js
@@ -0,0 +1,9 @@
+const toggleMenu = () => {
+  const hamburger = document.querySelector("#hamburger");
+  const menu = document.querySelector(".navbar-menu");
+  hamburger.addEventListener("click", () => {
+    menu.classList.toggle("show-menu");
+  });
+};
+
+toggleMenu();
